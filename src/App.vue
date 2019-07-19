77b7c2c93554f80f05cbc933a995b3b6bbc88c57
JasonBai007 <byygkcg@163.com>
2019-07-19 16:48:50 +0800

    修改配置，添加小球

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrap">
+      <div class="ball" @click="enlarge" :style="styleObj"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      radius: 10
+    };
+  },
+  computed: {
+    styleObj() {
+      return {
+        width: this.radius + "vh",
+        height: this.radius + "vh"
+      };
+    }
+  },
+  methods: {
+    enlarge() {
+      let r = this.radius;
+      this.radius = r + 1;
+    }
   }
-}
+};
 </script>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+}
+.wrap {
+  width: 100%;
+  height: 50vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  border-bottom: 1px solid #999;
+  overflow: hidden;
+}
+.ball {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  background: #197869;
+  transition: all 0.2s;
+  &:hover {
+    cursor: pointer;
+  }
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+// Config Reference: https://cli.vuejs.org/zh/config/
+
+module.exports = {
+  devServer: {
+    open: true, // 配置自动启动浏览器  
+  }
+}
