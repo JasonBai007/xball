6e22e99887e82af11a66c6e648112f72efcd42b8
JasonBai007 <byygkcg@163.com>
2019-07-19 17:40:40 +0800

    修改静态资源输出路径

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 // Config Reference: https://cli.vuejs.org/zh/config/
 
 module.exports = {
+  // 我部署在自己的码云上了，必须改这里
+  // https://cli.vuejs.org/zh/config/#publicpath
+  publicPath:'/xball/', 
   devServer: {
     open: true, // 配置自动启动浏览器  
   }
